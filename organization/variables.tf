c6d80f3ef2d5bd2b319554d02a16be498d918a9c
Camden Lindahl <camden.lindahl@gmail.com>
2026-01-27 19:13:44 -0500

    :organization import generated

---
+++ organization/variables.tf
@@ -0,0 +1,27 @@
+variable "proprietary_signals_email" {
+  type        = string
+  description = "Email address for Proprietary Signals account root user (must be globally unique)"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", var.proprietary_signals_email))
+    error_message = "Must be a valid email address."
+  }
+}
+
+variable "aws_service_access_principals" {
+  type        = list(string)
+  description = "AWS services that can be integrated with the organization"
+  default = [
+    "sso.amazonaws.com",  # IAM Identity Center
+  ]
+}
+
+variable "common_tags" {
+  type        = map(string)
+  description = "Common tags applied to all resources"
+  default = {
+    Organization = "Noise2Signal LLC"
+    ManagedBy    = "terraform"
+    Layer        = "organization"
+  }
+}
